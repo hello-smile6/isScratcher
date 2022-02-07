c3fb71f9835f1ed62cd314e1b1562c9b7474c0d9
9pfs <73048226+hello-smile6@users.noreply.github.com>
2022-02-07 01:38:07 +0000

    Commit working version 1! Server is online at https://isscratcher.9pfs.repl.co/

--- api.js
+++ api.js
@@ -1,15 +1,49 @@
 // DOM selector: #profile-data > div.box-head > div > p > span.group
 const express=require("express");
 const validate=require("./validate.js");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fetch=require("cross-fetch");
+
 const findUsername=require("./findUsernameInPath.js");
 const api=function(req, res, next) {
   const username=findUsername(req.path.split("/"));
   if(validate(username)!==true) {
-    res.status(200);
-    res.end(username);
+    res.status(400);
+    res.end(JSON.stringify({error: "Invalid username",username: username}));
   }
   else {
-    const
+   /* res.sendStatus(501);
+    res.end();
+    return; */
+    fetch("https://scratch.mit.edu/users/".concat(username)).then(function(scratchRes) {
+      scratchRes.text().then(function(text) {
+        try {
+        const scratchDom=new JSDOM(text);
+        var value=scratchDom.window.document.querySelector("#profile-data > div.box-head > div > p > span.group").textContent.toLowerCase();
+          value=value.replaceAll("\n","");
+          value=value.replaceAll("\t","");
+          value=value.replaceAll(String.fromCharCode(32),"")
+        if(value=="scratchteam" || value=="scratcher") {
+          res.status(200);
+          res.end(JSON.stringify({isScratcher: true,username: username,value: value}));
+        }
+        else {
+          res.status(200);
+          res.end(JSON.stringify({isScratcher: false,username: username,value: value}));
+        }
+        }
+        catch(e) {
+          res.status(404);
+      res.send(JSON.stringify({error: "Not found",username: username,errorStack: e.stack}));
+          res.end();
+        }
+      },function(e) {res.status(404);
+      res.send(JSON.stringify({error: "Not found",username: username,errorStack: e.stack}));})
+    },function(e) {
+      res.status(404);
+      res.send(JSON.stringify({error: "Not found",username: username,errorStack: e.stack}));
+    })
   }
   
 }
