29905380bde7e89f6ce16e9789927858918265f8
9pfs <73048226+hello-smile6@users.noreply.github.com>
2022-02-07 01:49:56 +0000

    Add homepage

--- docs.js
+++ docs.js
@@ -1,1 +1,4 @@
 const express=require("express");
+const path=require("path");
+const docs=express.static(path.join(__dirname,"docs"));
+module.exports=docs;
--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const docs=require("./docs.js");
 const jsdom=require("jsdom");
 const express=require("express");
 const app=express();
+app.use("/",docs);
 app.use("/api",api);
 app.listen(8080);
